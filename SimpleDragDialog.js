fd632112323411c222586b36a5aaa066be52339d
eccenux <eccenux+git@gmail.com>
2026-04-11 22:57:43 +0200

    safe export

--- SimpleDragDialog.js
+++ SimpleDragDialog.js
@@ -1,202 +1,206 @@
-/**
- * Simple draggable dialog window.
- * 
- * To get an instance of this class use `dialog.uSdd`.
- */
-class SimpleDragDialog {
-	constructor() {
-		/** ID of the container. */
-		this.sddId = 'sdragdialog-dialogs';
-	}
-	show() {
-		this.dialog.classList.remove('hidden');
-		this.dialog.style.display = '';
-	}
-	hide() {
-		this.dialog.classList.add('hidden');
-		this.dialog.style.display = 'none';
-	}
-	/** Count of dialogs. */
-	dialogsCount() {
-		let sddContainer = document.getElementById(this.sddId);
-		if (!sddContainer) return 0;
-		return sddContainer.querySelectorAll('.sdragdialog-dialog:not(.hidden)').length;
-	}
+/* global mw */
+{
 	/**
-	 * Creates draggable dialog window.
-	 * @param {Object} opt
-	 * @param {string} opt.title
-	 * @param {string|HTMLElement} opt.content
-	 * @param {string|HTMLElement} opt.dialogClass Extra class.
-	 * @returns {HTMLElement} Dialog element.
+	 * Simple draggable dialog window.
+	 * 
+	 * To get an instance of this class use `dialog.uSdd`.
 	 */
-	create({content = '', title = '', dialogClass = '', startHidden = true} = {}) {
-		// container
-		const dialog = document.createElement('div'); // OR dialog?
-		this.dialog = dialog;
-		if (startHidden) {
-			this.hide();
+	class SimpleDragDialog {
+		constructor() {
+			/** ID of the container. */
+			this.sddId = 'sdragdialog-dialogs';
 		}
-		dialog.classList.add('sdragdialog-dialog');
-		if (dialogClass) {
-			dialog.classList.add(dialogClass);
+		show() {
+			this.dialog.classList.remove('hidden');
+			this.dialog.style.display = '';
 		}
-
-		// self reference
-		dialog.uSdd = this;
-
-		//
-		// header (draggeble)
-		const header = document.createElement('div');
-		header.className = 'u-header';
-
-		const closeBtn = document.createElement('button');
-		closeBtn.textContent = '×';
-		closeBtn.style.marginLeft = '10px';
-		closeBtn.addEventListener('click', ()=>{
-			dialog.dispatchEvent(new CustomEvent('dialog:close', {
-				detail: { reason: 'button' },
-			}));
-			dialog.remove();
-		});
-
-		const titleEl = document.createElement('span');
-		titleEl.textContent = title;
-
-		header.appendChild(titleEl);
-		header.appendChild(closeBtn);
-
-		//
-		// content
-		const body = document.createElement('div');
-		body.className = 'u-body';
-
-		if (typeof content === 'string') {
-			body.innerHTML = content;
-		} else {
-			body.appendChild(content);
+		hide() {
+			this.dialog.classList.add('hidden');
+			this.dialog.style.display = 'none';
 		}
-
-		//
-		// elements
-		this.dialog = dialog;
-		this.header = header;
-		this.body = body;
-
-		//
-		// finalize
-		dialog.appendChild(header);
-		dialog.appendChild(body);
-		const sddId = this.sddId;
-		let sddContainer = document.getElementById(sddId);
-		if (!sddContainer) {
-			sddContainer = document.createElement('div');
-			sddContainer.setAttribute('id', sddId);
-			document.body.appendChild(sddContainer);
+		/** Count of dialogs. */
+		dialogsCount() {
+			let sddContainer = document.getElementById(this.sddId);
+			if (!sddContainer) return 0;
+			return sddContainer.querySelectorAll('.sdragdialog-dialog:not(.hidden)').length;
 		}
-		sddContainer.appendChild(dialog);
+		/**
+		 * Creates draggable dialog window.
+		 * @param {Object} opt
+		 * @param {string} opt.title
+		 * @param {string|HTMLElement} opt.content
+		 * @param {string|HTMLElement} opt.dialogClass Extra class.
+		 * @returns {HTMLElement} Dialog element.
+		 */
+		create({content = '', title = '', dialogClass = '', startHidden = true} = {}) {
+			// container
+			const dialog = document.createElement('div'); // OR dialog?
+			this.dialog = dialog;
+			if (startHidden) {
+				this.hide();
+			}
+			dialog.classList.add('sdragdialog-dialog');
+			if (dialogClass) {
+				dialog.classList.add(dialogClass);
+			}
 
-		this.makeDraggable(dialog, header);
-		if (this.isMobile()) {
-			this.enableDoubleTapMax(dialog, header);
-			dialog.style.left = '0';
-			dialog.style.right = '0';
-			dialog.style.top = '0';
-		}
+			// self reference
+			dialog.uSdd = this;
 
-		return dialog;
-	}
+			//
+			// header (draggeble)
+			const header = document.createElement('div');
+			header.className = 'u-header';
 
-	isMobile() {
-		return window.innerWidth < 420;
-	}
+			const closeBtn = document.createElement('button');
+			closeBtn.textContent = '×';
+			closeBtn.style.marginLeft = '10px';
+			closeBtn.addEventListener('click', ()=>{
+				dialog.dispatchEvent(new CustomEvent('dialog:close', {
+					detail: { reason: 'button' },
+				}));
+				dialog.remove();
+			});
 
-	/**
-	 * Adds double tap logic to window.
-	 * @param {HTMLElement} dialog The dialog to move.
-	 * @param {HTMLElement} header Tap/maximize handle.
-	 */
-	enableDoubleTapMax(dialog, header) {
-		const DOUBLE_TAP_DELAY = 300; // ms
-		let inTap = false;
-		let title = header.querySelector('span');
-		title.style.userSelect = 'none';
-		title.addEventListener("touchstart", (e) => {
-			if(!inTap) {
-				inTap = true;
-				setTimeout(() => {
-					inTap = false;
-				}, DOUBLE_TAP_DELAY);
+			const titleEl = document.createElement('span');
+			titleEl.textContent = title;
+
+			header.appendChild(titleEl);
+			header.appendChild(closeBtn);
+
+			//
+			// content
+			const body = document.createElement('div');
+			body.className = 'u-body';
+
+			if (typeof content === 'string') {
+				body.innerHTML = content;
 			} else {
-				e.preventDefault();
+				body.appendChild(content);
+			}
+
+			//
+			// elements
+			this.dialog = dialog;
+			this.header = header;
+			this.body = body;
+
+			//
+			// finalize
+			dialog.appendChild(header);
+			dialog.appendChild(body);
+			const sddId = this.sddId;
+			let sddContainer = document.getElementById(sddId);
+			if (!sddContainer) {
+				sddContainer = document.createElement('div');
+				sddContainer.setAttribute('id', sddId);
+				document.body.appendChild(sddContainer);
+			}
+			sddContainer.appendChild(dialog);
+
+			this.makeDraggable(dialog, header);
+			if (this.isMobile()) {
+				this.enableDoubleTapMax(dialog, header);
 				dialog.style.left = '0';
 				dialog.style.right = '0';
 				dialog.style.top = '0';
 			}
-		});
-	}
 
-	/**
-	 * Adds drag logic to window.
-	 * @param {HTMLElement} dialog The dialog to move.
-	 * @param {HTMLElement} header Drag handle.
-	 */
-	makeDraggable (dialog, header) {
-		let isDragging = false;
-		let offsetX = 0;
-		let offsetY = 0;
+			return dialog;
+		}
 
-		let title = header.querySelector('span');
+		isMobile() {
+			return window.innerWidth < 420;
+		}
 
-		// disable scroll
-		title.style.touchAction = 'none';
+		/**
+		 * Adds double tap logic to window.
+		 * @param {HTMLElement} dialog The dialog to move.
+		 * @param {HTMLElement} header Tap/maximize handle.
+		 */
+		enableDoubleTapMax(dialog, header) {
+			const DOUBLE_TAP_DELAY = 300; // ms
+			let inTap = false;
+			let title = header.querySelector('span');
+			title.style.userSelect = 'none';
+			title.addEventListener("touchstart", (e) => {
+				if(!inTap) {
+					inTap = true;
+					setTimeout(() => {
+						inTap = false;
+					}, DOUBLE_TAP_DELAY);
+				} else {
+					e.preventDefault();
+					dialog.style.left = '0';
+					dialog.style.right = '0';
+					dialog.style.top = '0';
+				}
+			});
+		}
 
-		title.addEventListener('pointerdown', (e) => {
-			// prevent text selection / scrolling on touch
-			title.setPointerCapture(e.pointerId);
+		/**
+		 * Adds drag logic to window.
+		 * @param {HTMLElement} dialog The dialog to move.
+		 * @param {HTMLElement} header Drag handle.
+		 */
+		makeDraggable (dialog, header) {
+			let isDragging = false;
+			let offsetX = 0;
+			let offsetY = 0;
 
-			isDragging = true;
-			offsetX = e.clientX - dialog.offsetLeft;
-			offsetY = e.clientY - dialog.offsetTop;
-		});
+			let title = header.querySelector('span');
 
-		document.addEventListener('pointermove', (e) => {
-			if (!isDragging) return;
+			// disable scroll
+			title.style.touchAction = 'none';
+
+			title.addEventListener('pointerdown', (e) => {
+				// prevent text selection / scrolling on touch
+				title.setPointerCapture(e.pointerId);
+
+				isDragging = true;
+				offsetX = e.clientX - dialog.offsetLeft;
+				offsetY = e.clientY - dialog.offsetTop;
+			});
+
+			document.addEventListener('pointermove', (e) => {
+				if (!isDragging) return;
+				dialog.style.right = 'auto';
+				dialog.style.bottom = 'auto';
+				dialog.style.left = (e.clientX - offsetX) + 'px';
+				dialog.style.top = (e.clientY - offsetY) + 'px';
+			});
+
+			document.addEventListener('pointerup', (e) => {
+				title.releasePointerCapture(e.pointerId);
+
+				isDragging = false;
+			});
+		}
+
+		/**
+		 * Centers the dialog within the viewport.
+		 *
+		 * Can center horizontally, vertically, or both.
+		 *
+		 * @param {Object} [options]
+		 * @param {boolean} [options.x=true] Center horizontally (X axis).
+		 * @param {boolean} [options.y=true] Center vertically (Y axis).
+		 */
+		center ({x=true, y=true}={}) {
+			const dialog = this.dialog;
+
+			const rect = dialog.getBoundingClientRect();
+			let offsetX = x ? (window.innerWidth - rect.width) / 2 : 0;
+			let offsetY = y ? (window.innerHeight - rect.height) / 2 : 0;
+
 			dialog.style.right = 'auto';
 			dialog.style.bottom = 'auto';
-			dialog.style.left = (e.clientX - offsetX) + 'px';
-			dialog.style.top = (e.clientY - offsetY) + 'px';
-		});
-
-		document.addEventListener('pointerup', (e) => {
-			title.releasePointerCapture(e.pointerId);
-
-			isDragging = false;
-		});
+			dialog.style.left = offsetX + 'px';
+			dialog.style.top = offsetY + 'px';
+		}
 	}
 
-	/**
-	 * Centers the dialog within the viewport.
-	 *
-	 * Can center horizontally, vertically, or both.
-	 *
-	 * @param {Object} [options]
-	 * @param {boolean} [options.x=true] Center horizontally (X axis).
-	 * @param {boolean} [options.y=true] Center vertically (Y axis).
-	 */
-	center ({x=true, y=true}={}) {
-		const dialog = this.dialog;
-
-		const rect = dialog.getBoundingClientRect();
-		let offsetX = x ? (window.innerWidth - rect.width) / 2 : 0;
-		let offsetY = y ? (window.innerHeight - rect.height) / 2 : 0;
-
-		dialog.style.right = 'auto';
-		dialog.style.bottom = 'auto';
-		dialog.style.left = offsetX + 'px';
-		dialog.style.top = offsetY + 'px';
-	}
+	// export
+	window.SimpleDragDialog = SimpleDragDialog;
+	mw.hook('userjs.SimpleDragDialog.ready').fire(SimpleDragDialog);
 }
-
-// export
-window.SimpleDragDialog = SimpleDragDialog;
